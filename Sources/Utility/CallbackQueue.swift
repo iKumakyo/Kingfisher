bd1e34c8d8f10a46b9d3730cb892b46a09518b6c
ixongju <ixongju@gmail.com>
2019-05-27 23:40:05 +0900

    Update comment

--- Sources/Utility/CallbackQueue.swift
+++ Sources/Utility/CallbackQueue.swift
@@ -28,8 +28,8 @@ import Foundation
 
 /// Represents callback queue behaviors when an calling of closure be dispatched.
 ///
-/// - asyncMain: Dispatch the calling to `DispatchQueue.main` with an `async` behavior.
-/// - currentMainOrAsync: Dispatch the calling to `DispatchQueue.main` with an `async` behavior if current queue is not
+/// - mainAsync: Dispatch the calling to `DispatchQueue.main` with an `async` behavior.
+/// - mainCurrentOrAsync: Dispatch the calling to `DispatchQueue.main` with an `async` behavior if current queue is not
 ///                       `.main`. Otherwise, call the closure immediately in current main queue.
 /// - untouch: Do not change the calling queue for closure.
 /// - dispatch: Dispatches to a specified `DispatchQueue`.
